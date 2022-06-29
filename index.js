50e0690e5e38364337fff06ddb4634981180cdb5
Austin <acw0415@gmail.com>
2022-06-28 20:48:24 -0400

    commit tree d6366b45f004086d4cc1fdf20f57065fa19e1cf7

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let distanceInFeet = feet;
+  let charge;
+  if(distanceInFeet < 400){
+    charge = 0;
+    return "This one is on me!"
+  }
+    else if(distanceInFeet >= 400 && distanceInFeet <= 2000){
+    charge = 20;
+    return "That will be twenty bucks."
+  }
+    else if(distanceInFeet >= 2000 && distanceInFeet <= 2500){
+    charge = 30;
+    return "I will gladly take your thirty bucks."
+  }
+    else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":  
+    return "Thank you so much.";  
+      break;
+    case "not as generous":  
+    return "Thank you.";   
+      break;
+    default:  
+    return "Bye.";   
+    
+  }
 }
